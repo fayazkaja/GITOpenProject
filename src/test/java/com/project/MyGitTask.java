331cd6267212a553ca8a027620aea517027694ef
fayazkaja <fayazkaja@gmail.com>
2023-02-27 21:26:30 +0530

    First Push

--- src/test/java/com/project/MyGitTask.java
+++ src/test/java/com/project/MyGitTask.java
@@ -1,5 +1,13 @@
 package com.project;
 
 public class MyGitTask {
-
+	private void method1() {
+		System.out.println("Method 1");
+	}
+	private void method2() {
+		System.out.println("Method 2");
+	}
+	private void method3() {
+		System.out.println("Method 3");
+	}
 }
